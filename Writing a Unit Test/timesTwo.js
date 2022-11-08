7a74ceead4e629156ebd18d0d25a0d945ae67acb
Bahadır Coşkun <112313083+bhdrcskn@users.noreply.github.com>
2022-11-08 13:52:58 +0300

    Add files via upload

---
+++ Writing a Unit Test/timesTwo.js
@@ -0,0 +1,9 @@
+// Task 1: Code the timesTwo function declaration
+function timesTwo(number)
+{
+    return 2*number
+}
+
+// Task 2: Export the timesTwo function as a module
+export default timesTwo;
+
